d0425cca3a400af8f221583b8f2eab29e796ef80
Ben Rowe <brow495@gmail.com>
2021-03-19 13:39:35 -0600

    Add cypress and grid-layout.

--- index.js
+++ index.js
@@ -58,7 +58,7 @@ function loadTemplate(event) {
     
     //loading bar (in then?)
     
-    fetch('http://madlibz.herokuapp.com/api/random?minlength=1&maxlength=20')
+    fetch('http://madlibz.herokuapp.com/api/random?minlength=1&maxlength=25')
         .then(parseJSON)
         .then(displayTemplate)
         // .catch()
@@ -107,11 +107,6 @@ function createStory() {
         story = story + valueList[i] + ' ' + collectAnswers()[i]
     };
 
-    //     collectAnswers().forEach(answer => {
-    //         story = story + valueList[i] + ' ' + answer + ','
-    //         // story = story + story.concat(valueList[i] + ' ' + answer)
-    //     });
-    // };
     reset(instructions);
     resetMain();
     appendStory(story);
@@ -147,63 +142,3 @@ function optimisticRender(event) {
     
     event.target.reset();
 }
-
-
-
-    // console.log(template)
-    
-
-    // let responseList = [];
-
-    // template.blanks.forEach(input => {
-    //     const form = document.createElement('form');
-    //     formList.appendChild(form);
-    //     const li = document.createElement('li');
-    //     li.innerHTML = `<input name=${input} placeholder=${input} /> <br />`
-    //     const submitButton = document.createElement('li');
-    //     submitButton.innerHTML = "<input type='submit' />"
-    //     form.append(li, submitButton);
-    //     form.addEventListener('submit', () => {
-    //         event.preventDefault();
-    //         event.stopPropagation();
-    //         let formData = new FormData(form);
-    //         let value = formData.get(`${input}`);
-    //         responseList.push(value);
-    //         const answer = document.createElement('li')
-    //         answer.textContent = value
-    //         answerList.appendChild(answer);
-    //     });
-    // })
-    // console.log(responseList)
-
-    // template.blanks.forEach(input => {
-    //     const li = document.createElement('li');
-    //     li.innerHTML = `<input name=${input} placeholder=${input} /> <br />`
-    //     promptList.appendChild(li);
-    // });
-    // const li = document.createElement('li');
-    // li.innerHTML = "<input type='submit' />"
-    // promptList.appendChild(li);
-
-    // promptList.addEventListener('submit', () => {
-    //     event.preventDefault()
-    //     let formData = new FormData(promptList);
-    
-    //     console.log(formData)
-    
-
-    //     for (let i = 0; i < promptList.length; i++) {
-    //         responseList.push(formData.flatten[i]);
-    //     }
-    
-    //     // formData.values( value => {
-    //     //     for (let i = 0; i < formData.values.length; i++) {
-    //     //         responseList.push(value);
-    //     //     }
-    //     // }
-    //     // )
-    //     console.log(responseList)
-    //     // return responseList;
-    // })
-
-
